3739e0d75fac3dfa7ba9caaeebf99dffa1d5562b
Matt <893074711@qq.com>
2017-07-02 18:13:17 +0800

    finish login

---
+++ src/js/action.js
@@ -0,0 +1,3 @@
+$.fn.api.settings.api = {
+  'login' : 'http://123.207.168.112:10000/staff/login',
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,40 @@
-// $.fn.api.settings.api = {
-//   'login' : '/staff/login?account={account}&password={password}',
-// };
+$(document).ready(function () {
+    $('#login').form({
+        fields: {
+            account: {
+                identifier: 'account',
+                rules: [{
+                        type: 'empty',
+                        prompt: '邮箱不能为空'
+                    },
+                    {
+                        type: 'email',
+                        prompt: '请输入有效的邮箱'
+                    }
+                ]
+            },
+            password: {
+                identifier: 'password',
+                rules: [{
+                    type: 'empty',
+                    prompt: '密码不能为空'
+                }]
+            }
+        },
+        onSuccess: function (e) {
+            //阻止表单的提交
+            e.preventDefault();
+        }
+    }).api({
+        action: 'login',
+        method: 'POST',
+        serializeForm: true,
+        onSuccess: function(data) {
+            if(data.error != null){
+                $('#login').form('add errors',[data.error]);
+            }else{
+                alert(1);
+            }
+        },
+    });
+})
